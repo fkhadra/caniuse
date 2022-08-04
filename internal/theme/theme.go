b237f12cb6f3d1191eda15cca615ccb1133fd3e6
Fadi Khadra <fdkhadra@gmail.com>
2022-08-04 21:15:23 +0200

    yet another project

---
+++ internal/theme/theme.go
@@ -0,0 +1,36 @@
+package theme
+
+import "github.com/charmbracelet/lipgloss"
+
+var (
+	ErrorColor     = lipgloss.AdaptiveColor{Light: "#F23D5C", Dark: "#F23D5C"}
+	SuccessColor   = lipgloss.AdaptiveColor{Light: "#3DF222", Dark: "#3DF222"}
+	WarningColor   = lipgloss.AdaptiveColor{Light: "#f1c40f", Dark: "#f1c40f"}
+	InfoColor      = lipgloss.AdaptiveColor{Light: "#3498db", Dark: "#3498db"}
+	NeutralColor   = lipgloss.AdaptiveColor{Light: "#969B86", Dark: "#696969"}
+	HighlightColor = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#874BFD"}
+	MagentaColor   = lipgloss.AdaptiveColor{Light: "#ba007a", Dark: "#ba007a"}
+	SpinnerColor   = MagentaColor
+
+	Bold = lipgloss.NewStyle().Bold(true)
+
+	Text           = lipgloss.NewStyle().Bold(true)
+	TextInfo       = Text.Copy().Foreground(InfoColor)
+	TextError      = Text.Copy().Foreground(ErrorColor)
+	TextSuccess    = Text.Copy().Foreground(SuccessColor)
+	TextHightlight = Text.Copy().Foreground(HighlightColor)
+
+	Badge = lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#1a1a1a")).
+		Bold(true).
+		Padding(0, 1, 0, 1)
+	BadgeSuccess = Badge.Copy().Background(SuccessColor)
+	BadgeWarning = Badge.Copy().Background(WarningColor)
+	BadgeError   = Badge.Copy().Background(ErrorColor)
+	BadgeInfo    = Badge.Copy().Background(InfoColor)
+	BadgeNeutral = Badge.Copy().Background(NeutralColor).Foreground(lipgloss.Color("#fff"))
+
+	HelpKey = lipgloss.NewStyle().
+		Bold(true).
+		Foreground(InfoColor)
+)
