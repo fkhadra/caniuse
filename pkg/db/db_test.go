bb17e59c3d9cad93f3c2ad6c953ff74b4135ab15
Fadi Khadra <fdkhadra@gmail.com>
2022-08-05 23:52:07 +0200

    test: add happy path for db

---
+++ pkg/db/db_test.go
@@ -0,0 +1,38 @@
+package db_test
+
+import (
+	"errors"
+	"ganaiuse/pkg/db"
+	"os"
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func removeConfigDir() {
+	os.RemoveAll(db.DefaultConfigDir)
+}
+
+func TestDownloadDatabaseIfNotExist(t *testing.T) {
+	removeConfigDir()
+	_, err := os.Stat(db.DefaultConfigDir)
+
+	// ensure config is removed
+	assert.True(t, errors.Is(err, syscall.ENOENT))
+	d, err := db.Init()
+
+	assert.Nil(t, err)
+	assert.NotEmpty(t, d)
+}
+
+func TestCheckForUpdate(t *testing.T) {
+	d, _ := db.Init()
+
+	// make current db old
+	d.Updated = 0
+
+	updated, err := d.CheckForUpdate()
+	assert.Nil(t, err)
+	assert.True(t, updated)
+}
